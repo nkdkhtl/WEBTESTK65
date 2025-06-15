1d97162a17019ab26be1343f090cabee62f58054
Nam Khuc <khucphuongnam2005@gmail.com>
2025-06-15 09:13:46 +0700

    CHECK

--- Form validation/validator.js
+++ Form validation/validator.js
@@ -1,26 +1,36 @@
 const nameInput = document.querySelector("#input__name")
+const submitBtn = document.querySelector(".form-submit")
 
-function getMessage(msg,elm) {
+submitBtn.onclick = (e) => {
+    e.preventDefault()
+    checkEmpty(nameInput.value, nameInput)
+}
+
+function getMessage(msg, elm) {
     const msgElm = elm.nextElementSibling
     msgElm.textContent = msg
 }
-function toggleMessage(bool,elm) {
-    if (bool) {
+
+function toggleMessage(isValid, elm) {
+    if (isValid) {
         elm.parentElement.classList.remove("invalid")
-        getMessage("",elm)
+        getMessage("", elm)
     } else {
         elm.parentElement.classList.add("invalid")
-
     }
-    
 }
 
 nameInput.onchange = (e) => {
-    let value = e.target.value;
-    if (value.length <= 0) {
-        toggleMessage(false,nameInput)
-        getMessage("Vui lòng nhập tên của bạn",nameInput)
+    checkEmpty(e.target.value, nameInput)
+}
+
+function checkEmpty(val, elm) {
+    if (val.trim().length <= 0) {
+        toggleMessage(false, elm)
+        getMessage("Vui lòng không để trống!", elm)
     } else {
-        toggleMessage(true,nameInput)
+        toggleMessage(true, elm)
     }
 }
+/* Không chỉnh sửa bất kì đoạn code nào ở trên */
+/* Code từ đây */
